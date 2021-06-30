67a236c127569b38285da9ae43970d9046712cea
shridutt <shridutt270@gmail.com>
2021-07-01 00:49:58 +0530

    removed dummy data for testing

--- src/Pages/TimeTable/TimeTableView.js
+++ src/Pages/TimeTable/TimeTableView.js
@@ -7,70 +7,6 @@ import TodayTimeTable from "../../components/TodayTimeTable";
 import { AuthContext } from "../../shared/context/auth-context";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 
-const dummy_data_today = [
-  {
-    _id: "60db2868123be979a66f10b8",
-    professor: "60daf170bbdce06fdc1ede45",
-    day: 1,
-    classRoom: {
-      _id: "60daeaa0c574bb6de58c20c3",
-      name: "1-A",
-    },
-    slotNumber: 0,
-    __v: 0,
-  },
-  {
-    _id: "60db2a80a103357a19094279",
-    professor: "60daf170bbdce06fdc1ede45",
-    day: 1,
-    classRoom: {
-      _id: "60daeaa0c574bb6de58c20c3",
-      name: "1-A",
-    },
-    slotNumber: 1,
-    __v: 0,
-  },
-  null,
-  null,
-  null,
-  null,
-];
-
-const dummy_data_week = [
-  [
-    {
-      _id: "60db2868123be979a66f10b8",
-      professor: "60daf170bbdce06fdc1ede45",
-      day: 1,
-      classRoom: {
-        _id: "60daeaa0c574bb6de58c20c3",
-        name: "1-A",
-      },
-      slotNumber: 0,
-      __v: 0,
-    },
-    {
-      _id: "60db2a80a103357a19094279",
-      professor: "60daf170bbdce06fdc1ede45",
-      day: 1,
-      classRoom: {
-        _id: "60daeaa0c574bb6de58c20c3",
-        name: "1-A",
-      },
-      slotNumber: 1,
-      __v: 0,
-    },
-    null,
-    null,
-    null,
-    null,
-  ],
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null],
-];
-
 const useStyles = makeStyles((theme) => ({
   mt: {
     marginTop: theme.spacing(2),
@@ -100,8 +36,6 @@ const TimeTableView = (props) => {
 
   const weekClickHandler = async () => {
     setLoadTimeTableFor("week");
-    //TODO: call API for week's lecture
-    //setlectures(dummy_data_week);
     try {
         const resData = await sendRequest("http://localhost:8080/api/lecture/week/" + auth.userType + "/" + auth.userId, "GET", null, { "authorization": auth.token, "Content-Type": "application/json" });
         setWeekLecture(resData.lectures);
